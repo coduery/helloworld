92e6eb403995c031daf9974c002e79f20d22d301
Gavin Rouleau <grconnectivity@gmail.com>
2018-01-13 13:48:19 -0700

    initial commit

---
+++ src/main/java/hello/HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package hello.HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Jenkins World");
+	}
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+}
